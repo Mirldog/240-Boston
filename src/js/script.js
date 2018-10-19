473a7848e71ad58b58733066ae2601b13e9984a5
mirldog <kyle.mirley@gmail.com>
2018-10-19 19:19:51 -0400

    pages fleshed out

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+function validateForm() {
+    var x = document.forms["OrderForm"]["customerName"].value;
+    if (x == null || x == "") {
+        document.forms["OrderForm"]["customerName"].style.backgroundColor = 'pink';
+        return false;
+    }
+}
---
+++ src/pages/process.php
@@ -0,0 +1,14 @@
+<?php
+    echo 'Data Submitted'
+
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Survey</title>
+    <link href="css/styles04.css" rel="stylesheet">
+</head>
+<body>
+    <h1>Thank you for completing the form</h1>
+</body>
